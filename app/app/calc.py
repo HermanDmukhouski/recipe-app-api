9829a18569d817921c6273a6da501409bd4012fc
HermanDmukhouski <herman.dmukhouski@digitalinspirationlabs.io>
2023-05-16 18:51:00 +0300

    fix: edit format with autopep8

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,8 +4,9 @@ Calculator functions
 
 
 def add(x, y):
-    """Add x and y and return result."""    
+    """Add x and y and return result."""
     return x + y
+
 
 def subtract(x, y):
     """Subtract x from y and return result."""
--- app/app/tests.py
+++ app/app/tests.py
@@ -2,33 +2,22 @@
 Sample tests
 """
 
+
 from django.test import SimpleTestCase
 
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
-    
+
     def test_add_nubmers(self):
         """Test adding numbers together."""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subrtact_numbers(self):
         """Test subracting numbers."""
         res = calc.subtract(10, 15)
-        
+
         self.assertEqual(res, 5)
-        
-        
- 
-
-
-  
-
-
-
-
-
-
-
